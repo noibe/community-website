ea8877e044c8e8844756c7a82e918378ff1ede04
SamuelMoraesF <samuel@samuelmoraes.com>
2015-01-23 19:00:06 -0200

    Custom Styles for Theme

--- styles.php
+++ styles.php
@@ -4,4 +4,10 @@
 	background-color:#000000;
 	color:#<?php header_textcolor(); ?>;
 }
+#page p {
+	font-size: 1.5em;
+    font-weight: 300;
+    text-align: center;
+    margin-top: 20px;
+}
 </style>
