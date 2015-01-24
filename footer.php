39b02e8afb656f08ff031bc56c7dc824e5edabe6
SamuelMoraesF <samuel@samuelmoraes.com>
2015-01-23 22:48:46 -0200

    footer

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
-<footer id="footer" class="black block"></footer>
+<footer id="footer" class="font-white blue-a400 block footer">
+
+&copy; <?php echo date("Y") ?> - Família <b>Noibe</b>
+
+</footer>
 
 <script src="<?php echo get_template_directory_uri(); ?>/js/mowe.js"></script>
 <?php wp_footer(); ?>
